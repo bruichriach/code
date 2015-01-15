48df550b12953122e4e71c9219c8c3353be0376b
forshaw <forshaw@atm.ox.ac.uk>
2015-01-15 12:56:29 +0000

    Allocated Fields (not elementally)

--- src/main.F90
+++ src/main.F90
@@ -3,6 +3,7 @@ program sw
  use parallel
  use grid
  use params
+ use variables
  
   implicit none
   
@@ -20,6 +21,48 @@ program sw
   call split_domain(mx, my, lx, nx, ly, ny, north, south, east, west)
   
   call set_grids()
+  
+
+ call create_field(s,.false.)
+ 
+ call create_field(depth,.true.)
+ call create_field(mindepth,.false.)
+ 
+ call create_field(h,.true.)
+ call create_field(u,.true.)
+ call create_field(v,.true.)
+ 
+ call create_field(h_tmp,.false.)
+ call create_field(u_tmp,.false.)
+ call create_field(v_tmp,.false.)
+ 
+ call create_field(ke,.true.)
+ call create_field(ape,.false.)
+ 
+ call create_field(zeta,.false.)
+ 
+ call create_field(lapu,.true.)
+ call create_field(lapv,.true.)
+ 
+ call create_field(smag,.true.)
+ call create_field(tension,.true.)
+ call create_field(strain,.false.)
+ 
+ call create_field(smagu,.false.)
+ call create_field(smagv,.false.)
+ 
+ call create_field(q,.false.)
+  
+ call create_field(m,.true.)
+ call create_field(minm,.false.)
+ 
+ call create_field(bfricu,.false.)
+ call create_field(bfricv,.false.)
+ 
+ call create_field(utau,.false.)
+ call create_field(vtau,.false.)
+ 
+ call create_field(f,.false.)
   
   
   
